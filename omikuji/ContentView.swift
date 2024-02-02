8b109e55c887a714cf8bcef45449b0e38cb72b9e
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-02 11:23:05 +0900

    Initial Commit

---
+++ omikuji/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  omikuji
+//
+//  Created by 木村朋広 on 2024/02/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ omikuji/omikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  omikujiApp.swift
+//  omikuji
+//
+//  Created by 木村朋広 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct omikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
